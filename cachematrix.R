b8d618af5b78c61091bb9e8f9e0076bd78b8f62e
Josef Eisl <zapster@zapster.cc>
2015-08-20 23:29:11 +0200

    Add alternative solution (makeCacheMatrix2).

--- cachematrix.R
+++ cachematrix.R
@@ -47,3 +47,41 @@ cacheSolve <- function(x, ...) {
     x$setInv(inv)
     inv
 }
+
+
+## Alternative solution to the cache matrix problem:
+##
+## The problem with the two method solution is that objects
+## created by makeCacheMatrix expose the $setInv method which
+## allows manipulation of the inverted matrix. Everyone could
+## use the method to set it to an arbitrary value with would
+## make the object inconsistent (i.e. x$getInv() does not
+## return the inverse matrix of x$get()).
+##
+## The solution proposed below moves the logic of cacheSolve
+## into the $getInv member function. The $setInv member is
+## no longer needed. The cached matrix is guaranteed to be
+## consistent (if nobody messes around with the environment).
+##
+## In Object Oriented Programming this strategy might be
+## called encapsulation and data protection.
+makeCacheMatrix2 <- function(m = matrix()) {
+    inv <- NULL
+    set <- function(x) {
+        m <<- x
+        inv <<- NULL
+    }
+    get <- function() {
+        m
+    }
+    getInv <- function(...) {
+        if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+        }
+        message("calculating inverse matrix")
+        inv <<- solve(m, ...)
+        inv
+    }
+    list(set = set, get = get, getInv = getInv)
+}
